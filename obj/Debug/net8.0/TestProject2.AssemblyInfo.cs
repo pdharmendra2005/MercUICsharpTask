f67716a3b378f93ce578dcfeff6604207682d33e
Anthony Soares <C95023446@utcain.com>
2025-10-18 21:21:55 +0100

    git ignore

--- obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
+++ obj/Debug/net8.0/TestProject2.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+9c54f4a6f8ecb9c767c871e04ad8a1e7ffdecaf2")]
 [assembly: System.Reflection.AssemblyProductAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyTitleAttribute("TestProject2")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
